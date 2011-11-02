2c564362da9675be3fbf4d197d28dd1e14814a40
Anderson de Oliveira <anderson.o@gmail.com>
2011-11-02 19:45:32 -0200

    Refactory da busca. Removendo da classe de obra

---
+++ src/java/edu/ritter/bibliotex/Search.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ritter.bibliotex;
+
+/**
+ *
+ * @author anderson
+ */
+public class Search {
+    
+}
